d754142ca30d9228e45682b4db83dd1d8b7abf52
mo3tsem11 <moatsemashraf514@gmail.com>
2025-10-19 06:29:55 +0300

    Add files via upload

---
+++ ass5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    if (num2 == 0) {
+        printf("Cannot divide by zero.\n");
+    } 
+    else if (num1 % num2 == 0) {
+        printf("%d is a multiple of %d.\n", num1, num2);
+    } 
+    else {
+        printf("%d is not a multiple of %d.\n", num1, num2);
+    }
+
+    return 0;
+}
